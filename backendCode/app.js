22bd5fb9cb293233e9bfb08f5d58ff391d7a01e1
Gopika2299 <gopikasivasakthik@gmail.com>
2023-08-20 21:47:47 +0530

    error

--- backendCode/app.js
+++ backendCode/app.js
@@ -1,34 +1,3 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const travelerRoutes = require('../routers/travellerRoutes');
-const propertyOwnerRoutes = require('../routers/propertyOwnerRoutes');
-const adminRoutes = require('../routers/adminRouter');
+const app = require('./app/config/express')
 
-
-const app = express();
-
-// Middleware
-app.use(bodyParser.json());
-
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://Gopikak:Gopika22@cluster0.bl3vrlo.mongodb.net/TravelAndBooking', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(error => console.error('MongoDB connection error:', error));
-console.log('check')
-// API Routes
-app.use('/travelers', travelerRoutes);
-app.use('/property-owners', propertyOwnerRoutes);
-app.use('/admin', adminRoutes);
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'An error occurred' });
-});
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app
